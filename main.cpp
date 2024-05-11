8ad71e78d0338013ce7352bf573e2f5f2ca2dc99
Jayden <hongkang@hongkang.name>
2024-05-11 14:49:35 -0400

    dev: main loop

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <cstdlib>
+#include <queue>
 
 #include "src/event.h"
+
 
 #define NUM_AUDIO_SOURCE 1
 #define NUM_VIDEO_SOURCE 1
 #define NUM_DATA_SOURCE 1
 
-#define SOURCE_EVENTS_LENGTH 1000
+#define SOURCE_EVENTS_LENGTH 1000000
 
 #define Q_SIZE 100
 
@@ -21,37 +23,32 @@ enum EventType {
     DEPARTURE = 2
 };
 
-float sim_clock, clock_last_event;
+float clock_system, clock_last_event;
 ServerStatus server_status;
-float q[Q_SIZE + 1];
-int num_in_q;
+std::queue<Event *> q_arrival;
+std::queue<Event *> q_departure;
 
 int event_index;
 Event *arrival_event;
-float clock_departure_event;
 
 int num_delayed;
 float total_of_delays, area_num_in_q, area_server_status;
 
 EventType next_event_type;
 
-
+// snapshot of the system at clock 0
 void initialize(Event *events) {
     /* system state */
-
     // simulator clock
-    sim_clock = 0.0;
+    clock_system = 0.0;
     // server status
     server_status = IDLE;
-    // q
-    num_in_q = 0;
     // time of last event
     clock_last_event = 0.0;
 
     // event source
     event_index = 0;
     arrival_event = &events[event_index];
-    clock_departure_event = 1.0e+30;
     // index move to next event
     event_index++;
 
@@ -63,64 +60,128 @@ void initialize(Event *events) {
 }
 
 void step(Event *events) {
-    // update system clock
-    if (arrival_event->clock < clock_departure_event) {
+    // decide next event type
+    if (0 == q_departure.size()) {
         next_event_type = ARRIVAL;
-        sim_clock = arrival_event->clock;
     } else {
-        next_event_type = DEPARTURE;
-        sim_clock = clock_departure_event;
+        Event *departure_head = q_departure.front();
+        if (arrival_event->clock < departure_head->clock) {
+            next_event_type = ARRIVAL;
+        } else {
+            next_event_type = DEPARTURE;
+        }
     }
-    // TODO update server status
-    server_status = BUSY;
-    // TODO update number in q
-    num_in_q = 0;
-    // update time of last event
-    clock_last_event = sim_clock;
-    // update arrival event
-    arrival_event = &events[event_index];
-    event_index++;
-    // TODO update departure clock
-    clock_departure_event = 0.0;
 
     printf("%d\n", event_index);
 }
 
 
 void update_time_avg_stats(void) {
-    float time_since_last_event;
-
-    /* Compute time since last event, and update last-event-time marker. */
-
-    time_since_last_event = sim_clock - clock_last_event;
-    clock_last_event = sim_clock;
-
-    /* Update area under number-in-queue function. */
-
-    area_num_in_q += num_in_q * time_since_last_event;
-
-    /* Update area under server-busy indicator function. */
-
-    area_server_status += server_status * time_since_last_event;
 }
 
-void arrive(void)  /* Arrival event function. */
-{
-    // to be completed by students
+void arrive(Event *events) {
+    if (IDLE == server_status) {
+        clock_system = arrival_event->clock;
+        // update arrival event
+        arrival_event = &events[event_index];
+        event_index++;
+
+        // create a departure event
+        Event *departure_event = (Event *) malloc(sizeof(Event));
+        memset(events, 0, sizeof(Event));
+        // TODO simulate depart interval according to packet type
+        departure_event->clock = arrival_event->clock + 0.2;
+        departure_event->packet = arrival_event->packet;
+        q_departure.push(departure_event);
+
+        // since server is IDLE, this event will be served without pushing in q_arrival, update num delayed
+        num_delayed += 1;
+        total_of_delays += 0.0;
+        // calculate area_num_in_q
+        float clock_between_last_event = clock_system - clock_last_event;
+        area_num_in_q += clock_between_last_event * q_arrival.size();
+        // calculate area server status
+        area_server_status += int(server_status) * clock_between_last_event;
+
+        // update last event
+        clock_last_event = clock_system;
+        // update server status
+        server_status = BUSY;
+        // number in q_arrival no changes
+        // q_arrival no changes
+    } else {
+        clock_system = arrival_event->clock;
+        // update arrival event
+        arrival_event = &events[event_index];
+        event_index++;
+
+        // no event served, num delayed no change
+        num_delayed += 0;
+        // total delays no change
+        total_of_delays += 0.0;
+        // calculate area_num_in_q
+        float clock_between_last_event = clock_system - clock_last_event;
+        area_num_in_q += clock_between_last_event * q_arrival.size();
+        // calculate area server status
+        area_server_status += int(server_status) * clock_between_last_event;
+
+        // update last event
+        clock_last_event = clock_system;
+        // server keep busy
+        // update q_arrival
+        if (q_arrival.size() >= Q_SIZE) {
+            // TODO drop event
+        } else {
+            // create a departure event
+            Event *departure_event = (Event *) malloc(sizeof(Event));
+            memset(events, 0, sizeof(Event));
+            // TODO simulate depart interval according to packet type
+            departure_event->clock = arrival_event->clock + 0.2;
+            departure_event->packet = arrival_event->packet;
+            q_departure.push(departure_event);
+
+            q_arrival.push(arrival_event);
+        }
+    }
 }
 
 
-void depart(void)  /* Departure event function. */
-{
-    // to be completed by studnts
+void depart(void) {
+    Event *departure_head = q_departure.front();
+
+    clock_system = departure_head->clock;
+
+    // move out event from q_arrival
+    Event *arrival_head = q_arrival.front();
+
+    // calculate area_num_in_q
+    float clock_between_last_event = clock_system - clock_last_event;
+    area_num_in_q += clock_between_last_event * q_arrival.size();
+    // calculate area server status
+    area_server_status += int(server_status) * clock_between_last_event;
+
+    // update last event
+    clock_last_event = clock_system;
+
+    q_departure.pop();
+
+    // delay in q_arrival
+    if (q_arrival.size() > 0) {
+        num_delayed += 1;
+        float event_delay = clock_system - arrival_head->clock;
+        total_of_delays += event_delay;
+        q_arrival.pop();
+    } else {
+        if (0 == q_departure.size()) {
+            server_status = IDLE;
+        }
+    }
 }
 
 
-void report(void)  /* Report generator function. */
-{
-    /* Compute and write estimates of desired measures of performance. */
-
-    // to be completed by students
+void report(void) {
+    printf("num_delayed: %d, total_of_delays: %f, area_num_in_q: %f, area_server_status: %f ", num_delayed,
+           total_of_delays, area_num_in_q, area_server_status);
 }
 
 int main() {
@@ -160,7 +221,7 @@ int main() {
 
         switch (next_event_type) {
             case ARRIVAL:
-                arrive();
+                arrive(events);
                 break;
             case DEPARTURE:
                 depart();
